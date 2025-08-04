9847ff5351041ba06c60e83e6403de977c6fa7d8
Anastasia <nastya.il03@mail.ru>
2025-08-04 21:41:33 +0400

    New branch

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,19 +1,33 @@
 public class Radio {
     private int currentRadioStationNumber;   // от 0 до 9
     private int currentVolume;  //от 0 до 100
+    private int stationCount;
+    private int minVolume = 0;
+    private int maxVolume = 100;
+
+    public Radio() {
+        this.stationCount = 10;
+    }
+
+    public Radio(int stationCount) {
+        this.stationCount = stationCount;
+    }
+
 
     //сетеры и гетеры для станции
     public int getCurrentRadioStationNumber() {
         return currentRadioStationNumber;
     }
     public void setCurrentRadioStationNumber(int newCurrentRadioStationNumber) {
-        if (newCurrentRadioStationNumber >=0 && newCurrentRadioStationNumber <= 9) {
+        if (newCurrentRadioStationNumber >= 0 && newCurrentRadioStationNumber < stationCount) {
             currentRadioStationNumber = newCurrentRadioStationNumber;
+        } else {
+            currentRadioStationNumber = 0;
         }
     }
 
     public void next() {
-        if (currentRadioStationNumber < 9) {
+        if (currentRadioStationNumber >= 0 && currentRadioStationNumber < stationCount - 1) {
             currentRadioStationNumber = currentRadioStationNumber + 1;
         } else {
             currentRadioStationNumber = 0;
@@ -24,13 +38,15 @@ public class Radio {
         if (currentRadioStationNumber > 0) {
             currentRadioStationNumber = currentRadioStationNumber - 1;
         } else {
-            currentRadioStationNumber = 9;
+            currentRadioStationNumber = stationCount - 1;
         }
     }
 
     public void setRadioStationNumber(int station) {
-        if (station >= 0 && station<= 9) {
+        if (station >= 0 && station < stationCount) {
             currentRadioStationNumber = station;
+        } else {
+            currentRadioStationNumber = 0; // сброс на 0
         }
     }
 
@@ -40,20 +56,22 @@ public class Radio {
         return currentVolume;
     }
     public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume >= 0 && newCurrentVolume <= 100) {
+        if (newCurrentVolume >= minVolume && newCurrentVolume <= maxVolume) {
             currentVolume = newCurrentVolume;
+        } else {
+            currentVolume = 0;
         }
     }
 
     public void increaseVolume() {
-        if (currentVolume < 100) {
+        if (currentVolume < maxVolume) {
             currentVolume = currentVolume + 1;
         }
 
     }
 
     public void decreaseVolume() {
-        if (currentVolume > 0) {
+        if (currentVolume > minVolume) {
             currentVolume = currentVolume - 1;
         }
     }
